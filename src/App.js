f6579999bbdff56446ff15a6cc1b01b178e66e3a
Berkay Şahin <berkaynihas@gmail.com>
2023-11-09 17:38:05 +0300

    check

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import NavBar from './components/NavBar';
 import TodoWrapper from './components/TodoWrapper';
+import Login from './pages/Login';
 
 function App() {
+
   return (
     <div className="App">
       <NavBar />
-      <TodoWrapper />
+      <Routes>
+        <Route path='/' element={<TodoWrapper />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Login.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+const Login = () => {
+  return (
+    <form>
+      
+    </form>
+  )
+}
+
+export default Login
